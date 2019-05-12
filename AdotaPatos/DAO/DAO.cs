5a67611e28b06710dff298452b9031ce28a5f550
luis_pombal@hotmail.com <lhgpb157@gmail.com>
2019-05-12 12:14:06 -0300

    Crud Usuario finalizado

--- AdotaPatos/DAO/DAO.cs
+++ AdotaPatos/DAO/DAO.cs
@@ -4,17 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace AdotaPatos.DAO
-{
-    public class DAO
-    {
+namespace AdotaPatos.DAO {
+    public class DAO {
 
         public static MySqlConnection GetMySqlConnection(bool open = true,
-           bool convertZeroDatetime = false, bool allowZeroDatetime = false)
-        {
-            string cs = "Server=127.0.0.1;Database=projeto_ong_adota;Uid=root;Pwd=1234567;";
-            var csb = new MySqlConnectionStringBuilder(cs)
-            {
+           bool convertZeroDatetime = false, bool allowZeroDatetime = false) {
+            string cs = "Server=127.0.0.1;Database=projeto_ong_adota;Uid=root;Pwd=;";
+            var csb = new MySqlConnectionStringBuilder(cs) {
                 AllowZeroDateTime = allowZeroDatetime,
                 ConvertZeroDateTime = convertZeroDatetime
             };
---
+++ AdotaPatos/DAO/UsuarioDAO.cs
@@ -0,0 +1,44 @@
+﻿using AdotaPatos.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdotaPatos.DAO {
+    public class UsuarioDAO : DAO {
+
+        public void Salvar(Usuario usuario) {
+            using (var sqlConnection = GetMySqlConnection()) {
+                var query = @"insert into usuario (Id, Login, Cargo, Senha)values(@Id, @Login, @Cargo, @Senha)";
+                sqlConnection.Execute(query, usuario);
+            }
+        }
+
+        public IEnumerable<Usuario> Listar() {
+            using (var sqlConnection = GetMySqlConnection()) {
+                return sqlConnection.Query<Usuario>(@"select Id, Login, Cargo, Senha from usuario");
+            }
+        }
+
+        public Usuario PorId(long id) {
+            using (var sqlConnection = GetMySqlConnection()) {
+                return sqlConnection.Query<Usuario>(@"select Id, Login, Cargo, Senha from usuario where Id = @id", new { Id = id }).First();
+            }
+        }
+
+        public void Delete(long? id) {
+            using (var sqlConnection = GetMySqlConnection()) {
+                var query = @"delete from usuario where Id = @id";
+                sqlConnection.Execute(query, new { Id = id });
+            }
+        }
+
+        public void Atualizar(Usuario usuario) {
+            using (var sqlConnection = GetMySqlConnection()) {
+                var query = @"update usuario set Login = @Login, Cargo = @Cargo, Senha = @Senha  where Id = @Id";
+                sqlConnection.Execute(query, usuario);
+            }
+        }
+    }
+}
